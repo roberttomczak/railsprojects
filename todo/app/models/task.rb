c007bb8fb3c575b6f43ae90eb2d6467457f27a82
Robert Tomczak <r0b3rt2@wp.pl>
2012-11-20 09:21:41 +0100

    Usuniecie niepotrzebnych plikow

--- todo/app/models/task.rb
+++
@@ -1,3 +0,0 @@
-class Task < ActiveRecord::Base
-  attr_accessible :imie, :status
-end
--- todo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Todo::Application.config.secret_token = '821a13b3df4a7310b70d1d49d7e992932287eef44969227c1a588202f3730e486c6c120108d6bd9f776883bbc091ee7efb885598d241b4d148b8fccd2d6247ff'
