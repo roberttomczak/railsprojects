18cc517e22509249b06294788c982173f6b59e7b
Robert Tomczak <r0b3rt2@wp.pl>
2012-11-22 15:26:38 +0100

    Zmiana root'a z index na home, dodanie przycisku na home do index

--- addressbook/config/routes.rb
+++ addressbook/config/routes.rb
@@ -51,8 +51,9 @@ Addressbook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'books#index'
-  match '/home' => 'pages#home'
+  #root :to => 'books#index'
+  #match '/home' => 'pages#home'
+  root :to => 'pages#home'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
