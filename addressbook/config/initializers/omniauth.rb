22dc6660ab1e2cebb9c8262828d3c7ebbc358605
Robert Tomczak <r0b3rt2@wp.pl>
2012-12-04 08:14:56 +0100

    Dodanie autoryzacji

---
+++ addressbook/config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'NvEeyTpDeDsb2U6n1SHtkQ', 'mKlfgDov1E2xe9HmiYQAoJ2UppEJ2lsIA7WVqkHQk'
+end
--- addressbook/db/schema.rb
+++ addressbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121125151226) do
+ActiveRecord::Schema.define(:version => 20121128102615) do
 
   create_table "books", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(:version => 20121125151226) do
     t.datetime "updated_at", :null => false
     t.float    "latitude"
     t.float    "longitude"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
