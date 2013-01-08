993000544005195b3624dc6733c5e0f31462242c
Robert Tomczak <r0b3rt2@wp.pl>
2013-01-08 12:52:54 +0100

    Heroku

--- addressbook/config/initializers/omniauth.rb
+++ addressbook/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'NvEeyTpDeDsb2U6n1SHtkQ', 'mKlfgDov1E2xe9HmiYQAoJ2UppEJ2lsIA7WVqkHQk'
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'] , ENV['TWITTER_CONSUMER_SECRET']
 end
