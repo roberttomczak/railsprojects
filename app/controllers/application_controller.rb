07961c673136f7cdb4058bc20e3867e042026a52
Robert Tomczak <r0b3rt2@wp.pl>
2013-01-18 14:06:07 +0100

    Commit what need to be commited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,15 @@ class ApplicationController < ActionController::Base
 helper_method :current_user
 
 private
-
 def current_user
   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+end
+
+private
+def login_required
+  unless current_user
+    flash[:alert] = "You must first log in or register before accessing this page."
+    redirect_to root_path
+  end
 end
 end
